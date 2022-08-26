28c1c8b2584fc03a6193859e16ceea737c9e98a6
Kokularaj <kokulraj000@gmail.com>
2022-08-26 11:15:05 +0530

    news

---
+++ newswebsite/resources/views/home.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>News Website</title>
+        <link rel="stylesheet" href="assets/style.css">
+    </head>
+    <body>
+        <ul>
+ 
+
+
+            <li><a href="addnews">News</a></li>
+            <li><a href="home">Home</a></li>
+
+          </ul>
+          <div class="heading">
+            <marquee behavior="scroll" direction="right"><h2>Welcome to Hot News</h2></marquee>
+          </div>
+          <h2  align =center>Head News</h2>
+          <div class ="gallery">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <table>
+                <tr>
+            <td><img src="assets/images/image1.jpg" width="420px" height="400px">&nbsp;&nbsp;&nbsp;</td>
+            <td><img src="assets/images/image2.jpg" width="420px" height="400px">&nbsp;&nbsp;&nbsp;</td>
+            <td><img src="assets/images/image3.jpg" width="420px" height="400px"></td>
+        </tr>
+        <tr>
+            <td><a href="https://www.newsfirst.lk/">Breaking News</a></td>
+            <td><a href="https://www.dailynews.lk/category/political">Political News</a></td>
+            <td><a href="https://www.w3newspapers.com/srilanka/">News Papers</a></td>
+        </tr>
+        </table>
+          </div>
+          <div class="sport">
+            <h2 align =center>Sports</h2>
+            <div class ="sports">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <table>
+                    <tr>
+                <td><img src="assets/images/sports1.jpg" width="420px" height="400px">&nbsp;&nbsp;&nbsp;</td>
+                <td><img src="assets/images/sports2.jpg" width="420px" height="400px">&nbsp;&nbsp;&nbsp;</td>
+                <td><img src="assets/images/sports3.jpg" width="420px" height="400px"></td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.newsfirst.lk/category/sports/">Srilanka Sport News</a></td>
+                    <td><a href="https://www.dailynews.lk/category/sports/">Sports News Paper</a></td>
+                    <td><a href="https://www.skysports.com/">All Sports News</a></td>
+                </tr>
+            </table>
+              </div>
+
+          </div>
+          <div class="travel">
+            <h2 align =center>Travels</h2>
+            <div class ="travels">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <table>
+                    <tr>
+                <td><img src="assets/images/travel1.jpg" width="420px" height="400px">&nbsp;&nbsp;&nbsp;</td>
+                <td><img src="assets/images/travel2.jpg" width="420px" height="400px">&nbsp;&nbsp;&nbsp;</td>
+               <td> <img src="assets/images/travel3.jpeg" width="420px" height="400px"></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.newsfirst.lk/category/sports/">Srilanka Sport News</a></td>
+                <td><a href="https://www.dailynews.lk/category/sports/">Sports News Paper</a></td>
+                <td><a href="https://www.skysports.com/">All Sports News</a></td>
+            </tr>
+
+            </table>
+            </div>
+          </div>
+          <footer align = center>
+            CopyRighr @ 2022
+          </footer>
+
+
+    </body>
+</html>
+
